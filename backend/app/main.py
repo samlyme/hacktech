7bbe0172adc5f30ff939972e06c7cf78f0114845
Sam Ly <sam@samly.me>
2025-04-26 15:46:58 -0700

    separate user route

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,25 +4,19 @@ from typing import Annotated
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 
-from app.model import Token, User, UserBase, UserCreate
-from app.utils import ACCESS_TOKEN_EXPIRE_MINUTES, authenticate_user, create_access_token, get_current_user, get_password_hash
+from app.model import Token
+from app.utils import ACCESS_TOKEN_EXPIRE_MINUTES, authenticate_user, create_access_token
 from app.db import SessionDep, create_db_and_tables
 
-# to get a string like this run:
-# openssl rand -hex 32
-
-
-# fake_users_db = {
-#     "johndoe": {
-#         "username": "johndoe",
-#         "full_name": "John Doe",
-#         "email": "johndoe@example.com",
-#         "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
-#         "disabled": False,
-#     }
-# }
+from app.routes import users
 
 app = FastAPI()
+
+app.include_router(users.router)
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
 @app.post("/token")
 async def login_for_access_token(
@@ -41,37 +35,3 @@ async def login_for_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
     return Token(access_token=access_token, token_type="bearer")
-
-@app.post("/users", response_model=User)
-async def create_user(user: UserCreate, session: SessionDep):
-    hashed_pw = get_password_hash(user.password)
-    
-    user_in_db = User(
-        username=user.username,
-        email=user.email,
-        hashed_password=hashed_pw
-    )
-    
-    session.add(user_in_db)
-    session.commit()
-    session.refresh(user_in_db)
-    
-    return user_in_db
-
-
-@app.get("/users/me/", response_model=UserBase)
-async def read_users_me(
-    current_user: Annotated[User, Depends(get_current_user)],
-):
-    return current_user
-
-
-@app.get("/users/me/items/")
-async def read_own_items(
-    current_user: Annotated[User, Depends(get_current_user)],
-):
-    return [{"item_id": "Foo", "owner": current_user.username}]
-
-@app.on_event("startup")
-def on_startup():
-    create_db_and_tables()
---
+++ backend/app/routes/users.py
@@ -0,0 +1,39 @@
+from typing import Annotated
+from fastapi import APIRouter, Depends
+
+from app.db import SessionDep
+from app.model import User, UserBase, UserCreate
+from app.utils import get_current_user, get_password_hash
+
+
+router = APIRouter(prefix="/users", tags=["Users"])
+
+@router.post("/", response_model=User)
+async def create_user(user: UserCreate, session: SessionDep):
+    hashed_pw = get_password_hash(user.password)
+    
+    user_in_db = User(
+        username=user.username,
+        email=user.email,
+        hashed_password=hashed_pw
+    )
+    
+    session.add(user_in_db)
+    session.commit()
+    session.refresh(user_in_db)
+    
+    return user_in_db
+
+
+@router.get("/me/", response_model=UserBase)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    return current_user
+
+
+@router.get("/me/items/")
+async def read_own_items(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    return [{"item_id": "Foo", "owner": current_user.username}]
