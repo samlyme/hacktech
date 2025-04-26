87e37a393596bcb3da3153501e43b48d915c8171
Sam Ly <sam@samly.me>
2025-04-26 15:52:14 -0700

    separate token route

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,30 +8,13 @@ from app.model import Token
 from app.utils import ACCESS_TOKEN_EXPIRE_MINUTES, authenticate_user, create_access_token
 from app.db import SessionDep, create_db_and_tables
 
-from app.routes import users
+from app.routes import users, token
 
 app = FastAPI()
 
 app.include_router(users.router)
+app.include_router(token.router)
 
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
-
-@app.post("/token")
-async def login_for_access_token(
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-    session: SessionDep
-) -> Token:
-    user = authenticate_user(session, form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
-    return Token(access_token=access_token, token_type="bearer")
